bd9c402376443f847e1ecd159257e97680972ed8
6704062611255 <s6704062611255@email.kmutnb.ac.th>
2024-11-26 17:00:30 +0700

    ข้อ 2

--- lab1minx.c
+++ lab1minx.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 int main(){
 
-int x = 5;
-int y = 6;
+int x;
+int y;
+scanf("%d",&x);
+scanf("%d",&y);
 
 if (x>y){
-	printf("x is greater than y");
+	printf("x is greater than y \n");
+	printf("diff x>y = %d \n",(x-y));
 } else {
-	printf("y is greater than x");	
+	printf("y is greater than x \n");
+	printf("diff y > x = %d \n",(y-x));	
 }
 return 0;
 }
